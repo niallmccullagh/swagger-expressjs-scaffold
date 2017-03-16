052c97b90b0d14b1fa8ff1e132f88af0be8e39f5
Niall McCullagh <niall.mccullagh@gmail.com>
2017-03-16 23:11:32 +0000

    Manually map /v1/status to /status
    Status really should be at the base of the service, however swagger does
    not support overriding the base path for a particular
    resource/operation. This feature is coming in Open API specification.
    :-D
    
    In the meantime the route is manually declared.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = require('express')();
 const log4js = require('log4js');
 const bodyParser = require('body-parser');
 const errorHandler = require('./api/middleware/http500ErrorHandler');
+const statusController = require('./api/controllers/status');
 
 const appRoot = __dirname;
 
@@ -38,10 +39,15 @@ SwaggerExpress.create({ appRoot }, (err, swaggerExpress) => {
   const port = process.env.PORT || 10010;
   app.listen(port);
 
+
   if (swaggerExpress.runner.swagger.paths['/status']) {
-    logger.info(`\ntry this:\ncurl http://localhost:${port}/v1/status`);
+    // Map status to /status
+    app.get('/status', statusController.status);
+
+    logger.info(`\ntry this:\ncurl http://localhost:${port}/status`);
   }
 });
+
 
 if (process.env.swagger_mockMode || process.env.TEST_MODE === 'y') {
   logger.info('Running in mock mode');
